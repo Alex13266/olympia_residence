eee1400003f4d1eb7e2138e51965b246a6c5e7b3
Alex13266 <79878120+Alex13266@users.noreply.github.com>
2022-09-05 12:50:24 +0300

    05.09/ 3

---
+++ contacts.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://kit.fontawesome.com/f3465016f9.js"
+      crossorigin="anonymous"
+    ></script>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/css/line-awesome.min.css"
+      integrity="sha512-vebUliqxrVkBy3gucMhClmyQP9On/HAWQdKDXRaAlb/FKuTbxkjPKUyqVOxAcGwFDka79eTF+YXwfke1h3/wfg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/i18next@21.6.10/i18next.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-i18next@1.2.1/jquery-i18next.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/i18next-browser-languagedetector@6.1.3/i18nextBrowserLanguageDetector.min.js"></script>
+    <script type="module" src="loadTranslations.js"></script>
+    <link rel="stylesheet" href="typography.css" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="contacts.css" />
+    <link rel="stylesheet" href="mediaquery.css" />
+    <title>Olympia Residence</title>
+  </head>
+  <body>
+    <a href="#" class="to-top">
+      <i class="fa-solid fa-angle-up"></i>
+    </a>
+    <header class="contacts-navbar">
+      <nav id="navbarBg" class="navbar navbar-expand-lg fixed-top p-0">
+        <div class="container-fluid navbar-inner navbar-contacts">
+          <div class="navLogo p-2 ps-4">
+            <a href="./index.html"
+              ><img src="./images/siteLogo-white.png" alt=""
+            /></a>
+          </div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-center"
+            id="navbarNav"
+          >
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a
+                  data-i18n="Menu.Sales"
+                  id="navBlack"
+                  class="nav-link"
+                  href="#"
+                ></a>
+              </li>
+              <li class="nav-item">
+                <a
+                  data-i18n="Menu.Aboutproject"
+                  id="navBlack"
+                  class="nav-link"
+                  href="#"
+                ></a>
+              </li>
+              <li class="nav-item">
+                <a
+                  data-i18n="Menu.Gallery"
+                  id="navBlack"
+                  class="nav-link"
+                  href="#"
+                ></a>
+              </li>
+              <li class="nav-item">
+                <a
+                  data-i18n="Menu.Contacts"
+                  id="navBlack"
+                  class="nav-link"
+                  href="#"
+                ></a>
+              </li>
+            </ul>
+          </div>
+          <div class="MultilanguageMenu">
+            <select
+              class="MultilanguageBtn"
+              name="language"
+              id="languageSwitcher"
+            ></select>
+          </div>
+        </div>
+      </nav>
+      <div class="header-image">
+        <img src="./images/header2.jpg" alt="" />
+      </div>
+    </header>
+
+    <main>
+      <div class="contacts-info container=fluid my-5">
+        <div class="row maps-inner">
+          <div class="col maps-box d-grid">
+            <i class="fa-solid fa-phone"></i>
+            <h5>Телефон:</h5>
+            <p>089 991 0435</p>
+            <p>089 796 3030</p>
+          </div>
+          <div class="col maps-box d-grid">
+            <i class="fa-solid fa-location-dot"></i>
+            <h5>Адрес:</h5>
+            <p>ул. „Морски Звуци“ 20, 8238 Равда</p>
+          </div>
+          <div class="col maps-box d-grid">
+            <i class="fa-solid fa-envelope"></i>
+            <h5>Емайл:</h5>
+            <p>olympia_residence@abv.bg</p>
+            <p>olympia_residence@gmail.com</p>
+          </div>
+        </div>
+      </div>
+      <div class="container form-top">
+        <h2 class="contacts-heading">СВЪРЖЕТЕ СЕ СЕГА!</h2>
+        <div class="row justify-content-center">
+          <div
+            class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12"
+            style="width: 55%"
+          >
+            <div class="panel panel-danger">
+              <div class="panel-body">
+                <form
+                  method="post"
+                  id="reused_form"
+                  action="form.php"
+                  class="d-grid gap-3"
+                >
+                  <div class="form-group d-grid contact-box1">
+                    <div class="contact-name">
+                      <!-- <label>Вашето име *</label> -->
+                      <input
+                        type="text"
+                        name="name"
+                        required
+                        class="form-control"
+                        placeholder="Вашето име *"
+                      />
+                    </div>
+                    <!-- <div class="contact-phone">
+                      <label>Вашия телефон *</label>
+                      <input
+                        type="text"
+                        name="number"
+                        required
+                        class="form-control"
+                        placeholder="Вашия телефон *"
+                      />
+                    </div> -->
+                    <div class="form-group contact-email">
+                      <!-- <label>Вашия Email *</label> -->
+                      <input
+                        type="email"
+                        name="email"
+                        required
+                        class="form-control"
+                        placeholder="Вашия Email *"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group contact-message">
+                    <!-- <label>Съобщение</label> -->
+                    <textarea
+                      rows="3"
+                      name="message"
+                      class="form-control"
+                      placeholder="Съобщение"
+                    ></textarea>
+                  </div>
+                  <div class="form-group">
+                    <button
+                      class="btn btn-raised btn-lg btn-warning contact-submit"
+                      type="submit"
+                    >
+                      Изпрати
+                    </button>
+                    <?php if(isset($_GET['isEmailSent']) && $_GET['isEmailSent'] == 'true') : ?>
+                    <div
+                      id="success_message"
+                      
+                    >
+                      <h3>Съобщението беше изпратено успешно!</h3>
+                    </div>
+                    <?php endif; ?>
+                    <?php if(isset($_GET['isEmailSent']) && $_GET['isEmailSent'] == 'false') : ?>
+                    <div
+                      id="error_message"
+                     
+                    >
+                      <h3>Съобщението не успя да се изпрати!</h3>
+                    </div>
+                    <?php endif; ?>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="mapsInfo">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d925.6346647198488!2d27.68862982925537!3d42.650338985083415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6a4595313e11d8d6!2zNDLCsDM5JzAxLjIiTiAyN8KwNDEnMjEuMCJF!5e1!3m2!1sbg!2sbg!4v1661777989241!5m2!1sbg!2sbg"
+          width="100%"
+          height="360px"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </main>
+    <!-- Footer -->
+    <footer id="footer">
+      <div class="row footer-inner d-grid">
+        <div class="col footerBox">
+          <h6 class="pb-4 footer-headers">Нашата локация</h6>
+          <p class="footer-content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
+            architecto accusamus facere, delectus, veritatis voluptates quia
+            expedita, numquam unde aspernatur rerum officia ullam sit?
+          </p>
+          <div class="row footer-address d-grid pt-3 pb-1">
+            <div class="col">
+              <p class="footer-infoHeader">Адрес:</p>
+            </div>
+            <div class="col">
+              <p class="footer-infoContent" style="width: 90%">
+                ул. „Морски Звуци“ 20, 8238 Равда
+              </p>
+            </div>
+          </div>
+          <div class="row footer-phone d-grid py-1">
+            <div class="col">
+              <p class="footer-infoHeader">Телефон:</p>
+            </div>
+            <div class="col d-flex">
+              <p class="footer-infoContent">089 991 0435</p>
+              <span>&nbsp;/&nbsp;</span>
+              <p class="footer-infoContent">089 796 3030</p>
+            </div>
+          </div>
+          <div class="row footer-email d-grid py-1">
+            <div class="col">
+              <p class="footer-infoHeader">Имейл:</p>
+            </div>
+            <div class="col">
+              <p class="footer-infoContent" style="width: 70%">
+                olympia_residence@abv.bg
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col footerBox">
+          <h6 class="pb-4 footer-headers">Контакти на брокера</h6>
+          <div class="row footer-agentInfo d-grid">
+            <div class="col footer-imageBox">
+              <img src="./images/istockphoto-1186979596-612x612.jpg" alt="" />
+            </div>
+            <div class="col d-grid align-items-center">
+              <h5>Камелия Цифундарева</h5>
+            </div>
+          </div>
+          <span class="footer-line mb-3"></span>
+          <div class="row footer-agentPContacts d-grid mb-2">
+            <div class="col">
+              <i class="fa-solid fa-phone"></i>
+            </div>
+            <div class="col">
+              <p class="footer-infoContent" style="width: 28%">089 991 0435</p>
+            </div>
+          </div>
+          <div class="row footer-agentEContacts d-grid mb-3">
+            <div class="col">
+              <i class="fa-solid fa-envelope"></i>
+            </div>
+            <div class="col">
+              <p class="footer-infoContent" style="width: 51%">
+                olympia_beach@abv.bg
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col footerBox">
+          <h6 class="pb-5 footer-headers">Социални мрежи</h6>
+          <div class="row footer-socialMediaFB d-grid pb-3">
+            <div class="col">
+              <i class="fa-brands fa-facebook-f"></i>
+            </div>
+            <div class="col socialMedia-icons">
+              <a href="#">facebook.com</a>
+            </div>
+          </div>
+          <div class="row footer-socialMediaIG d-grid pb-3">
+            <div class="col">
+              <i class="fa-brands fa-instagram"></i>
+            </div>
+            <div class="col socialMedia-icons">
+              <a href="#">instagram.com</a>
+            </div>
+          </div>
+          <div class="row footer-socialMediaOB d-grid pb-3">
+            <div class="col">
+              <i class="fa-solid fa-earth-europe"></i>
+            </div>
+            <div class="col socialMedia-icons">
+              <a href="https://olympia-beach.com/">olympia_beach.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyrights d-grid justify-content-center">
+        <p>
+          © 2022 OLYMPIA RESIDENCE. Всички правила запазени | Общи правила
+        </p>
+      </div>
+    </footer>
+    <script src="navi.js"></script>
+    <script src="toTopPointer.js"></script>
+  </body>
+</html>
