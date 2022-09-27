c00bafdeb942f17840aacd004dd62528c3d810a1
Alex13266 <aleksivanov845@gmail.com>
2022-09-27 23:17:03 +0300

    27.09/ 1

--- dropdownColor.js
+++ dropdownColor.js
@@ -1,12 +1,26 @@
 function dropdownChange() {
-  let dropdown = document.getElementById("dropdown-Color");
-  let scrollValue = window.scrollY;
+    let dropdown = document.getElementById("dropdown-Color");
+    let scrollValue = window.scrollY;
 
-  if (scrollValue < 130) {
-    dropdown.classList.remove("dropdownMenu-colorchange");
-  } else {
-    dropdown.classList.add("dropdownMenu-colorchange");
-  }
+    if (scrollValue < 130) {
+        dropdown.classList.remove("dropdownMenu-colorchange");
+    } else {
+        dropdown.classList.add("dropdownMenu-colorchange");
+    }
 }
 
 window.addEventListener("scroll", dropdownChange);
+
+// Apartments ul
+
+let newDropdown = document.getElementsByClassName("newDropdown-Color");
+
+window.onscroll = function () {
+    for (i = 0; i < newDropdown.length; i++) {
+        if (window.scrollY < 130) {
+            newDropdown[i].classList.remove("dropdownMenu-colorchange");
+        } else {
+            newDropdown[i].classList.add("dropdownMenu-colorchange");
+        }
+    }
+};
