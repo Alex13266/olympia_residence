768772b479965bb5f1080393f116ae522007df96
Alex13266 <aleksivanov845@gmail.com>
2022-10-14 14:46:10 +0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,37 @@
+<?php
+
+    require 'includes/PHPMailer.php';
+	require 'includes/SMTP.php';
+	require 'includes/Exception.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+
+    $name = $_POST['name'];
+    $phone = $_POST['number'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "tls";
+    $mail->Port = "587";
+    $mail->Username = "olympia.residence2022@gmail.com";
+    $mail->Password = "kbvtkdyawgzxfeyq";
+    $mail->Subject = "Съобщение от Olympia Residence";
+    $mail->setFrom('olympia.residence2022@gmail.com');
+    $mail->isHTML(true);
+    $mail->Body = "<h1>Данни на потребителя:<br></h1></br><p>Име: $name<br></p><br><p>Телефон: $phone</p></br><p>Email: $email</p></br><p>Съобщение: $message</p>";
+    $mail->addAddress('aleks_1326@abv.bg');
+    $mail->CharSet="UTF-8";
+
+	if ( $mail->send() ) {
+        header('Location: /OlympiaResidence/contacts.php?isEmailSent=true');
+	}else{
+        header('Location: /OlympiaResidence/contacts.php?isEmailSent=false');
+	}
+	$mail->smtpClose();
+?>
